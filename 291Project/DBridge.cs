709f94a959dfa0ee78e320967e2d47e9fcd32b47
Andrew Numrich <numricha@mymacewan.ca>
2022-06-01 20:12:28 -0600

    added DBridge and System.Data.SqlClient nuget package

---
+++ 291Project/DBridge.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace _291Project
+{
+    public static class DBridge
+    {
+        public static bool initialized;
+        public static SqlConnection myConnection;
+        public static SqlCommand myCommand;
+        public static SqlDataReader myReader;
+
+        public static void init()
+        {
+            if(!initialized)
+            {
+                myConnection = new SqlConnection("Server = DESKTOP-BDONDJ3; Database = lab3_thursday; Trusted_Connection = yes");
+                myConnection.Open();
+                try
+                {
+                    myCommand = new SqlCommand();
+                    myCommand.Connection = myConnection;
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.ToString(), "Cound not connect to database.");
+                }
+            }
+        }
+
+        public static SqlDataReader run_query(String query)
+        {
+
+            return (new SqlCommand(query)).ExecuteReader();
+        }
+
+    }
+}
