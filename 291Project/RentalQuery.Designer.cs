66cd7ac6f9eedfc62826851eb3a4a7ca8179ee3a
Andrew Numrich <numricha@mymacewan.ca>
2022-06-01 22:15:22 -0600

    increase font size on rental request form

--- 291Project/RentalQuery.Designer.cs
+++ 291Project/RentalQuery.Designer.cs
@@ -42,76 +42,85 @@ namespace _291Project
             // 
             // comboBox1
             // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(311, 85);
+            this.comboBox1.Location = new System.Drawing.Point(311, 118);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(242, 40);
+            this.comboBox1.Size = new System.Drawing.Size(242, 53);
             this.comboBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(198, 88);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(163, 56);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 32);
+            this.label1.Size = new System.Drawing.Size(142, 45);
             this.label1.TabIndex = 1;
             this.label1.Text = "Car Type";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(311, 132);
+            this.dateTimePicker1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker1.Location = new System.Drawing.Point(311, 177);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(400, 39);
+            this.dateTimePicker1.Size = new System.Drawing.Size(400, 50);
             this.dateTimePicker1.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(236, 137);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(211, 182);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 32);
+            this.label2.Size = new System.Drawing.Size(94, 45);
             this.label2.TabIndex = 3;
             this.label2.Text = "From";
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(311, 177);
+            this.dateTimePicker2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker2.Location = new System.Drawing.Point(311, 239);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(400, 39);
+            this.dateTimePicker2.Size = new System.Drawing.Size(400, 50);
             this.dateTimePicker2.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(266, 182);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(252, 244);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(39, 32);
+            this.label3.Size = new System.Drawing.Size(53, 45);
             this.label3.TabIndex = 5;
             this.label3.Text = "To";
             // 
             // comboBox2
             // 
+            this.comboBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(311, 39);
+            this.comboBox2.Location = new System.Drawing.Point(311, 53);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(242, 40);
+            this.comboBox2.Size = new System.Drawing.Size(242, 53);
             this.comboBox2.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(218, 42);
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(189, 121);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 32);
+            this.label4.Size = new System.Drawing.Size(116, 45);
             this.label4.TabIndex = 7;
             this.label4.Text = "Branch";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(311, 223);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(311, 307);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 46);
+            this.button1.Size = new System.Drawing.Size(150, 79);
             this.button1.TabIndex = 8;
             this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
@@ -131,7 +140,7 @@ namespace _291Project
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Name = "RentalQuery";
-            this.Text = "Submit";
+            this.Text = "Rental Request";
             this.ResumeLayout(false);
             this.PerformLayout();
 
