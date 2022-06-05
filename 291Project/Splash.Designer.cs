3be332345ed6fc5f07b8e34a0ef4e050e085b668
postfen <26173007+postfen@users.noreply.github.com>
2022-06-04 21:17:58 -0600

    changing splash

--- 291Project/Splash.Designer.cs
+++ 291Project/Splash.Designer.cs
@@ -31,27 +31,28 @@
             this.Customer = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Customer
             // 
             this.Customer.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Customer.Location = new System.Drawing.Point(37, 91);
+            this.Customer.Location = new System.Drawing.Point(12, 40);
             this.Customer.Name = "Customer";
-            this.Customer.Size = new System.Drawing.Size(93, 38);
+            this.Customer.Size = new System.Drawing.Size(123, 53);
             this.Customer.TabIndex = 0;
-            this.Customer.Text = "Customer";
+            this.Customer.Text = "Request Reservation";
             this.Customer.UseVisualStyleBackColor = true;
             this.Customer.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(182, 91);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(95, 143);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 38);
+            this.button1.Size = new System.Drawing.Size(118, 26);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Employee";
+            this.button1.Text = "Employee Login";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_2);
             // 
@@ -60,20 +61,30 @@
             this.textBox1.BackColor = System.Drawing.SystemColors.Control;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(108, 25);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(289, 22);
             this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "Login:";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(178, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 53);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Sign Up";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Splash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(319, 167);
+            this.ClientSize = new System.Drawing.Size(317, 183);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Customer);
@@ -90,5 +101,6 @@
         private System.Windows.Forms.Button Customer;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
