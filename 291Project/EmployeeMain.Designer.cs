9a7cdd3050c193c261ffd02b153f52e6e2125304
grantbaartman <baartmang@mymacewan.ca>
2022-06-09 11:02:21 -0600

    color for panel on employee screen

--- 291Project/EmployeeMain.Designer.cs
+++ 291Project/EmployeeMain.Designer.cs
@@ -41,6 +41,7 @@
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.AllowDrop = true;
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(154)))), ((int)(((byte)(156)))));
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel1.Controls.Add(this.button2, 0, 2);
@@ -54,26 +55,26 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(163, 983);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 19F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(150, 922);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(6, 244);
+            this.button2.Location = new System.Drawing.Point(6, 230);
             this.button2.Margin = new System.Windows.Forms.Padding(6);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(158, 105);
+            this.button2.Size = new System.Drawing.Size(146, 98);
             this.button2.TabIndex = 2;
             this.button2.Text = "Exit";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(6, 127);
+            this.button4.Location = new System.Drawing.Point(6, 120);
             this.button4.Margin = new System.Windows.Forms.Padding(6);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(158, 105);
+            this.button4.Size = new System.Drawing.Size(146, 98);
             this.button4.TabIndex = 2;
             this.button4.Text = "Back";
             this.button4.UseVisualStyleBackColor = true;
@@ -83,17 +84,17 @@
             this.button1.Location = new System.Drawing.Point(6, 6);
             this.button1.Margin = new System.Windows.Forms.Padding(6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(158, 109);
+            this.button1.Size = new System.Drawing.Size(146, 102);
             this.button1.TabIndex = 1;
             this.button1.Text = "Home";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(175, 863);
+            this.button3.Location = new System.Drawing.Point(162, 809);
             this.button3.Margin = new System.Windows.Forms.Padding(6);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(158, 105);
+            this.button3.Size = new System.Drawing.Size(146, 98);
             this.button3.TabIndex = 3;
             this.button3.Text = "Cars";
             this.button3.UseVisualStyleBackColor = true;
@@ -102,19 +103,19 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(175, 21);
+            this.dataGridView1.Location = new System.Drawing.Point(162, 20);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 82;
             this.dataGridView1.RowTemplate.Height = 41;
-            this.dataGridView1.Size = new System.Drawing.Size(1190, 720);
+            this.dataGridView1.Size = new System.Drawing.Size(1098, 675);
             this.dataGridView1.TabIndex = 4;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // EmployeeMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1493, 983);
+            this.ClientSize = new System.Drawing.Size(1378, 922);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.tableLayoutPanel1);
