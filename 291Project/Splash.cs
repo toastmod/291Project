c4a0f2c6bdee49585d6313cd1f15ab785c50600c
postfen <26173007+postfen@users.noreply.github.com>
2022-06-11 16:22:12 -0600

    added some comments in the login code

--- 291Project/Splash.cs
+++ 291Project/Splash.cs
@@ -45,31 +45,29 @@ namespace _291Project
 
         private void button1_Click_3(object sender, EventArgs e)
         {
-
-            String userlogin = textBox1.Text;
+            // Following code is for logging in, and displaying a welcome message.
+            String userlogin = textBox1.Text; // take in the string from the text box
             String login_id = userlogin.Trim().ToUpper();
-            if (login_id == "")
+            if (login_id == "")// Error pop up when string is empty
             {
                 MessageBox.Show("Please Enter Valid Credentials", "Failed to Login",
                 MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            else if (login_id[0] == 'C')
+            else if (login_id[0] == 'C') // redirect to customer menu
             {
                 CustomerMainMenu cMain = new CustomerMainMenu();
                 cMain.CustomerDisplayName = userlogin;
                 cMain.Show();
-
                 this.Visible = false;
             }
-            else if (login_id[0] == 'E')
+            else if (login_id[0] == 'E') // redirect to employee menu
             {
-                // Links to employee screen.
                 EmployeeMain eMain = new EmployeeMain();
                 eMain.EmployeeDisplayName = userlogin;
                 eMain.Show();
                 this.Visible = false;
             }
-            else
+            else // Error if not beginning w C or E (temp)
             {
                 MessageBox.Show("Please Enter Valid Credentials", "Failed to Login",
                 MessageBoxButtons.OK, MessageBoxIcon.Error);
