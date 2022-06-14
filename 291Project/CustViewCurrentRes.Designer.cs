edb81d879dda1eb07e8ac472d44e37e707c10995
grantbaartman <baartmang@mymacewan.ca>
2022-06-14 09:40:25 -0600

    minor view current res change

--- 291Project/CustViewCurrentRes.Designer.cs
+++ 291Project/CustViewCurrentRes.Designer.cs
@@ -28,47 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.CustViewCurrentFilters = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(16, 10);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 72;
-            this.dataGridView1.RowTemplate.Height = 37;
-            this.dataGridView1.Size = new System.Drawing.Size(1321, 882);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // CustViewCurrentFilters
-            // 
-            this.CustViewCurrentFilters.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.CustViewCurrentFilters.Location = new System.Drawing.Point(16, 987);
-            this.CustViewCurrentFilters.Name = "CustViewCurrentFilters";
-            this.CustViewCurrentFilters.Size = new System.Drawing.Size(187, 73);
-            this.CustViewCurrentFilters.TabIndex = 1;
-            this.CustViewCurrentFilters.Text = "Filters";
-            this.CustViewCurrentFilters.UseVisualStyleBackColor = true;
             // 
             // CustViewCurrentRes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.CustViewCurrentFilters);
-            this.Controls.Add(this.dataGridView1);
             this.Name = "CustViewCurrentRes";
             this.Size = new System.Drawing.Size(1398, 1072);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button CustViewCurrentFilters;
     }
 }
